eb2842907a87fcf899d7d5969d522ebef5eda08b
JIMLU <kirpheaice@gmail.com>
2018-07-31 15:19:03 +0800

    Update

--- Test/src/j2se8/ch11/JoinDemo.java
+++ Test/src/j2se8/ch11/JoinDemo.java
@@ -3,10 +3,28 @@ package j2se8.ch11;
 public class JoinDemo
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		// TODO Auto-generated method stub
-
+		System.out.println("Main Thread Start.");
+		Thread t=new Thread(()->{
+			try
+			{
+				System.out.println("Sub Thread Start.");
+				for(int i=0;i<10;i++)
+				{
+					System.out.printf("Sub Thread %d %n",i);
+					Thread.sleep(500);
+				}
+				System.out.println("Sub Thread Stop.");
+			}
+			catch(Exception e)
+			{
+				
+			}
+		});
+		t.start();
+//		t.join();
+		System.out.println("Main Thread Stop.");
 	}
 
 }
